89f81964b349cec7eb295e61690258b661da87f5
Vitor Sousa <vguedes.sousa@gmail.com>
2019-01-01 16:04:31 -0200

    Init project

---
+++ src/main/java/br/com/vgsousa/pontointeligente/PontointeligenteApplication.java
@@ -0,0 +1,14 @@
+package br.com.vgsousa.pontointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontointeligenteApplication.class, args);
+	}
+
+}
+
